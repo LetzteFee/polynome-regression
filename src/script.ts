4155e78da29a44379f542b56c796daf313206fd2
LetzteFee <jannes.folkerts@protonmail.com>
2023-04-16 21:01:04 +0200

    fixed performance problem by adding variable to control calculations per frame per Polynom and moved imrpove() to Polynom to give access to calcDelta()

--- src/script.ts
+++ src/script.ts
@@ -18,20 +18,13 @@ class Point {
         fill('red');
         ellipse(x, y, 5);
     }
-    public drawLinearDelta(funk: Polynom): void {
-        let x: number = this.x * (width / WIDTH);
-        let y: number = height - (this.y * (height / HEIGHT));
-
-        stroke(0);
-        line(x, y, x, height - (funk.f(this.x) * (height / HEIGHT)));
-    }
     public toString(): string {
         let x: number = Math.round(this.x);
         let y: number = Math.round(this.y);
         return `(${x}, ${y})`;
     }
 }
-
+type Plot = Point[];
 class Koeffizient {
     public value: number;
     public sum: number;
@@ -39,46 +32,34 @@ class Koeffizient {
         this.value = v;
         this.sum = sum;
     }
-    private resetSum(): void {
+    public resetSum(): void {
         this.sum = 1;
     }
-    private increaseSum(): void {
+    public increaseSum(): void {
         this.sum *= 2;
     }
-    private decreaseSum(): void {
+    public decreaseSum(): void {
         this.sum *= 0.5;
     }
-    public improve(funk: Polynom, data: Point[]): void {
-        let origV: number = funk.calcCompleteDelta(data);
-
+    public increaseValue(): void {
         this.value += this.sum;
-        let plusV: number = funk.calcCompleteDelta(data);
-
-        this.value -= 2 * this.sum;
-        let minusV: number = funk.calcCompleteDelta(data);
-
-        this.value += this.sum;
-
-        if (plusV < origV) {
-            this.value += this.sum;
-            this.increaseSum();
-        } else if (minusV < origV) {
-            this.value -= this.sum;
-            this.increaseSum();
-        } else if (this.sum * 0.5 > 0) {
-            this.decreaseSum();
-            this.improve(funk, data);
-        } else {
-            this.resetSum();
-        }
+    }
+    public decreaseValue(): void {
+        this.value -= this.sum;
+    }
+    public sumTooLow(): boolean {
+        return !(this.sum * 0.5 > 0);
     }
 }
 
 class Polynom {
     private arr: Koeffizient[];
+    private training_data: Plot;
     private delta_expo: number;
-    private readonly color: color;
-    constructor(grad: number, expo: number, color: color) {
+    public readonly color: color;
+    private calculationsPerCall: number;
+    constructor(grad: number, expo: number, color: color, data: Plot, depth: number) {
+        this.training_data = data;
         this.arr = [];
         for (let i: number = 0; i <= grad; i++) {
             this.arr.push(new Koeffizient(0, 1));
@@ -86,11 +67,33 @@ class Polynom {
 
         this.delta_expo = expo;
         this.color = color;
+        this.calculationsPerCall = depth;
     }
-    public improve(data: Point[]): void {
-        for (let j: number = 0; j < 5000; j++) {
+    public improve(): void {
+        for (let j: number = 0; j < this.calculationsPerCall; j++) {
             for (let i: number = 0; i < this.arr.length; i++) {
-                this.arr[i].improve(this, data);
+                let origV: number = this.calcCompleteDelta();
+
+                this.arr[i].increaseValue();
+                let plusV: number = this.calcCompleteDelta();
+                this.arr[i].decreaseValue();
+
+                this.arr[i].decreaseValue();
+                let minusV: number = this.calcCompleteDelta();
+
+                this.arr[i].increaseValue();
+
+                if (plusV < origV) {
+                    this.arr[i].increaseValue();
+                    this.arr[i].increaseSum();
+                } else if (minusV < origV) {
+                    this.arr[i].decreaseValue();
+                    this.arr[i].increaseSum();
+                } else if (this.arr[i].sum * 0.5 > 0) {
+                    this.arr[i].decreaseSum();
+                } else {
+                    this.arr[i].resetSum();
+                }
             }
         }
     }
@@ -119,12 +122,24 @@ class Polynom {
             line(i, height - y1, i + 1, height - y2);
         }
     }
-    public calcCompleteDelta(data: Point[], expo: number = this.delta_expo): number {
+    public drawLinearDelta(): void {
+        for (let i: number = 0; i < this.training_data.length; i++) {
+            let x: number = this.training_data[i].x * (width / WIDTH);
+            let y: number = height - (this.training_data[i].y * (height / HEIGHT));
+
+            stroke(0);
+            line(x, y, x, height - this.f(this.training_data[i].x) * (height / HEIGHT));
+        }
+    }
+    public calcCompleteDelta(expo: number = this.delta_expo): number {
         let n: number = 0;
-        for (let i: number = 0; i < data.length; i++) {
-            n += Math.abs(data[i].y - this.f(data[i].x)) ** expo;
+        for (let i: number = 0; i < this.training_data.length; i++) {
+            n += Math.abs(this.training_data[i].y - this.f(this.training_data[i].x)) ** expo;
         }
         return n;
+    }
+    public calcCompleteLinearDelta(): number {
+        return this.calcCompleteDelta(1);
     }
     public getGrad(): number {
         return this.arr.length - 1;
@@ -132,32 +147,32 @@ class Polynom {
     public getExpo(): number {
         return this.delta_expo;
     }
-    public getColor(): string {
+    /*public colorToString(): string {
         return `r: ${this.color.r} g: ${this.color.g} b: ${this.color.b}`;
-    }
+    }*/
     public toString(): string {
-        return `${this.getColor()}: Grad: ${this.getGrad()}, Delta-Expo: ${this.getExpo()}, LinearDelta: ${this.calcCompleteDelta(VALUES, 1)}`;
+        return `Grad: ${this.getGrad()}, Delta-Expo: ${this.getExpo()}, Delta: ${this.calcCompleteDelta()}, LinearDelta: ${this.calcCompleteLinearDelta()}`;
     }
 }
 const WIDTH: number = 10;
 const HEIGHT: number = 100;
 
-let VALUES: Point[] = [];
+let VALUES: Plot = [];
 let funktionen: Polynom[] = [];
 
 function setup(): void {
     let GRAD: number = 6;
-    funktionen = [
-        //new Polynom(GRAD, 1, {r: 0, g: 0, b: 0}),
-        new Polynom(GRAD, 2, { r: 0, g: 255, b: 0 }),
-        //new Polynom(GRAD, 3, {r: 255, g: 0, b: 0}),
-        new Polynom(GRAD, 4, { r: 0, g: 0, b: 255 })
-    ];
-
     let VALUES_LENGTH: number = GRAD + 1;
     for (let i: number = 0; i < VALUES_LENGTH; i++) {
         VALUES.push(new Point(WIDTH / VALUES_LENGTH * i + WIDTH / VALUES_LENGTH * 0.5, random(HEIGHT)));
     }
+
+    funktionen = [
+        new Polynom(GRAD, 1, { r: 0, g: 0, b: 0 }, VALUES, 100),
+        new Polynom(GRAD, 2, { r: 0, g: 128, b: 0 }, VALUES, 1000),
+        new Polynom(GRAD, 3, { r: 255, g: 0, b: 0 }, VALUES, 1000),
+        new Polynom(GRAD, 4, { r: 0, g: 0, b: 255 }, VALUES, 1000)
+    ];
 
     createCanvas(windowWidth, windowHeight);
 }
@@ -165,18 +180,16 @@ function draw(): void {
     background(220);
     render();
     for (let i = 0; i < funktionen.length; i++) {
-        funktionen[i].improve(VALUES);
+        funktionen[i].improve();
     }
 }
 function render(): void {
     for (let i: number = 0; i < VALUES.length; i++) {
         VALUES[i].draw();
-        for (let j: number = 0; j < funktionen.length; j++) {
-            VALUES[i].drawLinearDelta(funktionen[j]);
-        }
     }
     for (let i = 0; i < funktionen.length; i++) {
         funktionen[i].drawGraph();
+        funktionen[i].drawLinearDelta();
     }
 
     /*let zeilen: string[] = [
@@ -189,7 +202,7 @@ function render(): void {
     ]*/
     for (let i: number = 0; i < funktionen.length; i++) {
         noStroke();
-        fill(0);
+        fill(funktionen[i].color.r, funktionen[i].color.g, funktionen[i].color.b);
         text(funktionen[i].toString(), 5, 15 + 20 * i);
     }
 }
